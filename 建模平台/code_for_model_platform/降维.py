77c07d415eb79f5a2da88efcc116bbf4ccdb1f3e
jason <15375703265@163.com>
2020-09-08 18:35:20 +0800

    add more new functions in the domain of feature decomposition: PCA_M

--- 建模平台/code_for_model_platform/降维.py
+++ 建模平台/code_for_model_platform/降维.py
@@ -29,7 +29,7 @@ def centralize(X):
     mean = np.mean(X, axis=0)
     return (X - mean)
 
-def PCA_M(X, n_components=1):
+def PCA_M(X, n_components=None):
     """
     created by Shaoming, Wang
     :param X: 需要降维的多维array, 要经过缺失值填充，否则会报错
@@ -45,12 +45,15 @@ def PCA_M(X, n_components=1):
     # step2 计算协方差矩阵
     cov_matrix = np.mat(np.cov(X_, rowvar=False))
     # step3 对协方差矩阵做正交分解 选取最大的 n_components 个特征值
-    eigen_values, eigen_vectors= np.linalg.eig(cov_matrix)
+    eigen_values, eigen_vectors = np.linalg.eig(cov_matrix)
     # todo 这里存在一个bug需要解决：当解特征方程时存在多重根时，
     # 存在一个特征值对应多个特征向量，此时这些特征向量并不是正交的，需要进行正交化
     # 但是因为本方法适用在建模相关性检验未通过的一组变量，因此可以暂不考虑这个问题
     sorted_indice = np.argsort(eigen_values)
-    indice = sorted_indice[:-(n_components+1):-1]
+    if not n_components:
+        indice = sorted_indice[::-1]
+    else:
+        indice = sorted_indice[:-(n_components+1):-1]
     # 构成一组新基
     base = eigen_vectors[:,indice]
     # 数据降维
@@ -58,8 +61,15 @@ def PCA_M(X, n_components=1):
     return result, var_s[indice]
 
 def get_percentage_(X, X_, model='variance'):
-    pass
 
-def get_col_variance(x):
-    pass
+    if model == 'variance':
+        X_ORI_VARIANCE = np.sum(X.apply(get_col_variance,axis=1))
+        X_APP_VARIANCE = get_col_variance(np.array(X_))
+        return X_APP_VARIANCE/X_ORI_VARIANCE
 
+def get_col_variance(X):
+    row_n = X.shape[0]
+    return 1/(row_n -1) * np.sum((X - np.mean(X))**2)
+
+def var_clus(X,eigenmax=1,maxclus=None):
+    pass
