7af463249f39ac0d3c4fe595cf2090cefadec117
jason <15375703265@163.com>
2020-09-22 14:37:11 +0800

    add a new method to calculate ks value

--- 建模平台/code_for_model_platform/KS值.py
+++ 建模平台/code_for_model_platform/KS值.py
@@ -4,10 +4,6 @@ Created on Tue Apr  3 10:31:30 2018
 
 @author: surface
 """
-
-from sklearn.metrics import roc_curve, auc  
-import matplotlib.pyplot as plt  
-from scipy.stats import ks_2samp
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
@@ -18,5 +14,23 @@ def get_ks(y_pred,y_true):
     get_ks1 = ks_2samp(y_pred[y_true==1], y_pred[y_true!=1]).statistic
     return get_ks1
 
-
+### 计算KS值
+def KS(df, score, target):
+    '''
+    :param df: 包含目标变量与预测值的数据集
+    :param score: 得分或者概率
+    :param target: 目标变量
+    :return: KS值
+    '''
+    total = df.groupby([score])[target].count()
+    bad = df.groupby([score])[target].sum()
+    all_ = pd.DataFrame({'total':total, 'bad':bad})
+    all_['good'] = all_['total'] - all_['bad']
+    all_[score] = all_.index
+    all_ = all_.sort_values(by=score,ascending=False)
+    all_.index = range(len(all_))
+    all_['badCumRate'] = all_['bad'].cumsum() / all_['bad'].sum()
+    all_['goodCumRate'] = all_['good'].cumsum() / all_['good'].sum()
+    KS = all_.apply(lambda x: x.badCumRate - x.goodCumRate, axis=1)
+    return max(KS)
 
