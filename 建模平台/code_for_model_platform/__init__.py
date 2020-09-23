3299c3bb06d6eba9c4ce87f0c0cb3260a6f34c09
jason <15375703265@163.com>
2020-09-23 11:07:25 +0800

    change file name into English

--- 建模平台/code_for_model_platform/__init__.py
+++ 建模平台/code_for_model_platform/__init__.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 14 11:04:23 2018
+Created on Tue Aug 14 11:04:23 2020
 
-@author: surface
+@author: shaoming, Wang
 """
 
 from .AUC_GINI_KS import *
-from .GINI系数 import *
-from .KS值 import *
-from .Logit图 import *
-from .交叉列联表 import *
-from .划分数据集 import *
-from .去重 import *
-from .变量函数转换 import *
-# from .基于卡方连续变量自动分箱 import *
-from .平衡样本 import *
-from .建模 import *
-from .排序 import *
-from .检验十等分 import *
-from .洛伦兹曲线 import *
-from .相关性检验 import *
-from .等分表 import *
-from .缺失同质性检验 import *
-from .缺失填补 import *
-from .衍生变量类别变量 import *
-from .衍生变量连续变量 import *
-from .建立评分卡 import *
-from .F1检验 import *
-from .饼图 import *
-from .划分类别变量与连续变量 import *
-from .连续变量自动处理 import *
-from .可视化连续变量层级 import *
+from .GINI import *
+from .KSvalue import *
+from .Logitplot import *
+from .cross_table import *
+from .divide_dataset import *
+from .drop_duplicate import *
+from .var_transfer import *
+# from .auto_bin_using_chi import *
+from .balance_sample import *
+from .modeling import *
+from .sort import *
+from .ten_split import *
+from .roc_curve import *
+from .corr_test import *
+from .dengfentable import *
+from .homogeneity_test import *
+from .fill_na_value import *
+from .deprive_cate_varr import *
+from .deprive_continuous_var import *
+from .score_card import *
+from .F1test import *
+from .pie_plot import *
+from .divide_cate_var_conti_var import *
+from .auto_deal_conti_var import *
+from .visualize_layers import *
 from .varcluster import *
-from .降维 import *
+from .decomposition import *
