a67f55fec66b5dc33d73dabf5529be4eeb87c657
jason <15375703265@163.com>
2020-09-23 11:08:11 +0800

    change file name into English

---
+++ 建模平台/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='modeling_platform',
+      version='1.0',
+      author='Jason',
+      author_email='657590764@qq.com',
+      description='the code for model building',
+      packages=['code_for_model_platform'])
