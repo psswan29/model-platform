d35a22603997317e4e025c4c9bfbe7cdce9ac2b0
jason <15375703265@163.com>
2020-09-27 18:20:00 +0800

    add a new method bin_using_ks

--- 建模平台/code_for_model_platform/auto_bin_using_chi.py
+++ 建模平台/code_for_model_platform/auto_bin_using_chi.py
@@ -15,7 +15,7 @@ from icecream import ic
 from scipy.stats import chi2_contingency
 import os
 
-def moto_binning(data_0, var_continu: str, y:str):
+def moto_binning_chi(data_0, var_continu: str, y:str):
     """
     #基于卡方分层
     #
@@ -70,8 +70,102 @@ def moto_binning(data_0, var_continu: str, y:str):
     new_var_final = new_var_dict[ix]
     return new_var_final, ix
 
+def bin_using_ks(var_continuous, y, threshold=0.05):
+    """
+    使用best——ks方法实现自动分箱
+    :param var_continuous:
+    :param y:
+    :param threshold: 阈值，
+    :return:
+    """
+
+
+    def bin_using_ks_(var_len, var_continuous, y, log, threshold):
+        """
+        使用递归的方法分箱
+        :param var_len: 要分箱的连续变量初始长度
+        :param var_continu: 每次迭代要分箱的连续变量数据
+        :param y: 每次迭代因变量数据
+        :return:
+
+        终止条件
+
+        1.下一步分箱后,最小的箱的占比低于设定的阈值(threshold常用0.05)
+        2.下一步分箱后,该箱对应的y类别全部为0或者1
+        3.下一步分箱后,因变量反馈率不单调
+
+        """
+
+
+        # 对输入变量进行处理，然后进行检验，保证
+        var_continuous = pd.DataFrame(var_continuous).values.reshape(-1,)
+        y = pd.DataFrame(y).values.reshape(-1,)
+        assert var_continuous.shape == y.shape
+        # 检验
+        if ((y == 1).sum()/var_len < threshold) or ((y ==0).sum()/var_len < threshold):
+            raise ImportError
+
+        value_set= set(var_continuous)
+
+        ks_list = [(i,get_ks((var_continuous<=i).astype(int), y))    for i in value_set]
+        value, ks = sorted(ks_list,key=lambda x: x[1],reverse=True)[0]
+        log.append(value)
+
+        scalar1 = (var_continuous < value)
+        scalar2 = (var_continuous >= value)
+
+        # 下一步分箱后,最小的箱的占比低于设定的阈值(threshold常用0.05)
+        if (scalar1.sum()/var_len < threshold) or (scalar2.sum()/var_len < threshold):
+            return log
+        # 下一步分箱后, 该箱对应的y类别全部为0或者1
+        if len(set(y[scalar1]))==1 or len(set(y[scalar2]))==1:
+            return log
+        # 下一步分箱后,bad rate 不单调
+        if monotonicity_test(var_continuous,y, log):
+            return log
+
+        return bin_using_ks(var_len, var_continuous[scalar1], y[scalar1], log, threshold), \
+               bin_using_ks(var_len, var_continuous[scalar2], y[scalar2], log, threshold)
+
+    # 反馈率单调性检验
+    def monotonicity_test(var_continuous,y, log):
+        values = []
+        log = sorted(log)
+        state = set()
+        for i,e in enumerate(log[:-1]):
+            if i == 0:
+                values.append(y[(var_continuous < log[i])].mean())
+                continue
+            values.append(y[(var_continuous >= log[i]) & (var_continuous < log[i+1])].mean())
+            if i == (len(log)-2):
+                values.append(y[(var_continuous >= log[i])].mean())
+        for i,v in enumerate(values[:-1]):
+            if values[i]>values[i+1]:
+                state.update(0)
+            else:
+                state.update(1)
+            if len(state)>1:
+                return False
+        return  True
+
+
+    log = []
+    var_len = var_continuous.shape[0]
+    bin_using_ks_(var_len, var_continuous, y, log, threshold)
+
+    return log
+
+
+
+
+from scipy.stats import ks_2samp
+def get_ks(y_pred,y_true):
+    #y_pred 目标变量预测值 y_true 目标变量真实值
+    get_ks1 = ks_2samp(y_pred[y_true==1], y_pred[y_true!=1]).statistic
+    return get_ks1
+
 
 if __name__ == '__main__':
-    df = pd.read_csv('../lucheng_data.csv', encoding='gbk')
-    output = moto_binning(df, 'WEIXIN_APP_NUM_M1', 'Y')
-    
+    df = pd.read_csv('./lucheng_data.csv', encoding='gbk')
+    output = moto_binning_chi(df, 'WEIXIN_APP_NUM_M1', 'Y')
+    output = bin_using_ks()
