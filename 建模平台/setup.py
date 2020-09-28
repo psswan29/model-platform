3353c3d38945915c606dd9f952d55e00e51d631d
jason <15375703265@163.com>
2020-09-28 11:29:41 +0800

    add more details to setup file, install_requires option

--- 建模平台/setup.py
+++ 建模平台/setup.py
@@ -1,8 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='modeling_platform',
       version='1.0',
       author='Jason',
       author_email='657590764@qq.com',
       description='the code for model building',
-      packages=['code_for_model_platform'])
+      install_requires=[
+          'scikit-learn>=0.2.0',
+          'matplotlib',
+          'seaborn',
+          'scipy',
+          'numpy',
+          'pandas>=1.0.0',
+          'statsmodels',
+          'sklearn2pmml',
+          'factor_analyzer'
+      ],
+
+      packages=find_packages())
