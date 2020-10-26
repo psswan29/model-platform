7c773ea951afb3ad16032abe39908a6938bcc583
jason <15375703265@163.com>
2020-10-26 16:34:00 +0800

    optimize the performance of auto-bin algorithm using multi-processing pool; after optimization, it takes only 73 seconds compared with 300 seconds before.

--- 建模平台/code_for_model_platform/auto_bin_using_chi.py
+++ 建模平台/code_for_model_platform/auto_bin_using_chi.py
@@ -11,9 +11,9 @@ ON Tue Aug 26 12:18:22 2020
 from scipy.stats import chisquare  
 import numpy as np
 import pandas as pd
-from icecream import ic
 from scipy.stats import chi2_contingency
-import os
+from multiprocessing import Pool
+
 
 def auto_deal_continua(var_continua_analyse_2, data_1):
     """
@@ -25,16 +25,27 @@ def auto_deal_continua(var_continua_analyse_2, data_1):
      var_continua_process: 变量处理过程
     """
 
-
     var_continua_for_model = []
     var_continua_process = {}
-    for j1 in var_continua_analyse_2:
-        auto_dict = moto_binning_chi(data_1, j1, 'Y')
-        new_col = j1 + '_1'
-        data_1[new_col] = auto_dict[0]
-        var_continua_process[j1] = auto_dict[1]
+
+    # 使用一个进程池来运行分箱算法
+    with Pool(5) as p:
+        bin_result_list = p.starmap(moto_binning_chi, [(data_1, col, 'Y') for col in var_continua_analyse_2])
+
+    for var, t in zip(var_continua_analyse_2, bin_result_list):
+        new_var_final, ix = t
+        new_col = var + '_1'
+        data_1[new_col] = new_var_final
+        var_continua_process[var] = ix
         var_continua_for_model.append(new_col)
-        # print(var_continua_process[j1])
+
+    # for j1 in var_continua_analyse_2:
+    #     auto_dict = moto_binning_chi(data_1, j1, 'Y')
+    #     new_col = j1 + '_1'
+    #     data_1[new_col] = auto_dict[0]
+    #     var_continua_process[j1] = auto_dict[1]
+    #     var_continua_for_model.append(new_col)
+    #     print(var_continua_process[j1])
 
     # 结果显示连续变量自动处理结果
     for i in var_continua_process.items():
