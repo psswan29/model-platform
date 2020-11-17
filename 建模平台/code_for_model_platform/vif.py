85a32095b610ffbe0ea2db63fe8f70080515adf5
jason <15375703265@163.com>
2020-11-17 18:49:08 +0800

    optimize the function of vif calculation

--- 建模平台/code_for_model_platform/vif.py
+++ 建模平台/code_for_model_platform/vif.py
@@ -1,18 +1,22 @@
 
 import pandas as pd
+import numpy as np
 
-def calulate_vif(X):
+def calulate_vif(X, threshold=100):
+    """在不存在多重共线性的情况下，方差扩大因子接近于1。
+    但是，实际上自变量之间总是或多或少地存在多重共线性，因而将方差扩大因子等于1作为评价共线性的标准是不现实的。
+    多重共线性越强，方差扩大因子就越大。一个易用的标准：
+    当VIF值大于10时，就认为变量之间具有强烈的多重共线性，不能接受。"""
     from statsmodels.stats.outliers_influence import variance_inflation_factor
-    X[X.shape[1]]=1
-    #vif
-    vif=[]
-    for i in range(X.shape[1]-1):
-        vif.append(variance_inflation_factor(X.values,i))
-    #result_out
-    yy=pd.DataFrame(X.columns[:-1,])
-    yy.rename(columns={0:"var_name"},inplace=True)
-    yy["vif"]=vif
-    print(yy)
+
+    vif=[variance_inflation_factor(X.values,i) for i in range(X.shape[1])]
+
+    yy=pd.DataFrame({'var_name': X.columns, 'vif':vif})
+    yy['vif_test'] = yy.vif >= threshold
+    pos = np.argmax(yy['vif'])
+
+    return yy, yy['var_name'][pos], yy['var_name'][yy['vif_test']]
+
 
 if __name__ == '__main__':
     df = pd.DataFrame([[15.9, 16.4, 19, 19.1, 18.8, 20.4, 22.7, 26.5, 28.1, 27.6, 26.3]
@@ -21,4 +25,4 @@ if __name__ == '__main__':
                           , [108.1, 114.8, 123.2, 126.9, 132.1, 137.7, 146, 154.1, 162.3, 164.3, 167.6]]).T
     columns = ["var1", "var2", "var3", "var4"]
     df.columns = columns
-    calulate_vif(df)
+    yy = calulate_vif(df)
